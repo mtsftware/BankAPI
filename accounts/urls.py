3418c2a80ea831d6bf8f166fde8e11e9fb7c8c21
mtseher <sehermuhammedtaha@gmail.com>
2024-08-18 03:12:10 +0300

    add slash

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('register', views.register_view, name='register'),
-    path('test_token', views.test_token_view, name='test-token'),
+    path('register/', views.register_view, name='register'),
+    path('test_token/', views.test_token_view, name='test-token'),
     path('profile/', views.UserDetailView, name='user-detail'),
     path('profile/my_accounts/', views.AccountListCreateView, name='account-list-create'),
     path('profile/my_accounts/<int:account_id>/', views.AccountDetailView, name='account-detail'),
